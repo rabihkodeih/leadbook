16d346645851e4143004264daef4736f12b65f73
Rabih Kodeih <rabih.kodeih@gmail.com>
2018-12-19 14:06:47 +0200

    Add separate directory for cralwed data

--- crawler/run_company_index.sh
+++ crawler/run_company_index.sh
@@ -1,4 +1,4 @@
-rm company_index.json
-scrapy crawl campany_index -o company_index.json -L INFO
-python beautify_json.py company_index.json
+rm ../data/company_index.json
+scrapy crawl campany_index -o ../data/company_index.json -L INFO
+python beautify_json.py ../data/company_index.json
 echo
--- crawler/run_company_profiles.sh
+++ crawler/run_company_profiles.sh
@@ -1,4 +1,4 @@
-rm company_profiles.json
-scrapy crawl campany_profiles -a input_file="./company_index.json" -o company_profiles.json -L INFO
-python beautify_json.py company_profiles.json
+rm ../data/company_profiles.json
+scrapy crawl campany_profiles -a input_file="../data/company_index.json" -o ../data/company_profiles.json -L INFO
+python beautify_json.py ../data/company_profiles.json
 echo
