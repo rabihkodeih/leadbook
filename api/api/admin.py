f1c795b07b54b68a0d1f6757aa3be12b813c37b0
Rabih Kodeih <rabih.kodeih@gmail.com>
2018-12-19 20:19:33 +0200

    Add model inlines in admin

--- api/api/admin.py
+++ api/api/admin.py
@@ -2,11 +2,26 @@ from django.contrib import admin
 from api.models import Company, Secretary, Director, Subsidiary
 
 
+class SecretaryInline(admin.TabularInline):
+    model = Secretary
+
+
+class DirectorInline(admin.TabularInline):
+    model = Director
+
+
+class SubsidiaryInline(admin.TabularInline):
+    model = Subsidiary
+
+
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('id', 'company_name', 'security_code', 'country', 'phone',
                     'npwp', 'ipo_date', 'board', 'sector', 'sub_sector')
     search_fields = ('company_name',)
+    inlines = [SecretaryInline,
+               DirectorInline,
+               SubsidiaryInline]
     ordering = ('company_name',)
 
 
