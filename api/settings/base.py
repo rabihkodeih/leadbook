d5c014eabc6c402fccd855f880d317273ef2ed37
Rabih Kodeih <rabih.kodeih@gmail.com>
2018-12-20 14:57:41 +0200

    Update base settings

--- api/settings/base.py
+++ api/settings/base.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'common.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'leadbook',
+        'USER': 'admin',
+        'PASSWORD': 'adminadmin',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    },
 }
 
 
