294cdd092d784ec1d639838cbdd02c723e1aa8b7
Rabih Kodeih <rabih.kodeih@gmail.com>
2018-12-20 14:02:02 +0200

    Update zappa deployment settings

--- api/settings/production.py
+++ api/settings/production.py
@@ -17,17 +17,19 @@ INSTALLED_APPS += ['storages']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-AWS_S3_HOST = 's3.us-east-2.amazonaws.com'
-
 AWS_STORAGE_BUCKET_NAME = 'zappa-leadbook-static-3incchlv8'
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_USE_SSL = True
 
 AWS_HEADERS = {'Cache-Control': 'max-age=86400', }
 
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # enf of file
